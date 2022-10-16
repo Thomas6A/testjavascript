fb11f2025e29c483d272bf9c61facd5ee1365f01
Thomas6A <thomas.baudrin@outlook.fr>
2022-10-16 11:33:46 +0200

    en tete

--- testheader.js
+++ testheader.js
@@ -1,13 +1,21 @@
-window.onscroll = function() {myFunction()}
+window.onscroll = function() {myFunction()};
 
-
-function myFunction(){
-   if (window.scrolltop > document.getElementById("head").scrolltop)
-   {
-      document.getElementsByClassName("head").classlist.add('cache');
-   }
-  else
-   {
-    document.getElementsByClassName("head").classlist.remove('cache');
-   }
+function myFunction() {
+  if (document.documentElement.scrollTop != 0) {
+    document.getElementById("header").className = "cache";
+  } else {
+    document.getElementById("header").className = "";
   }
+}
+
+function afficher(){
+   document.getElementById("recherche").className = "afficher";
+   document.getElementById("croix").className = "afficher";
+   document.getElementById("loupe").className = "afficher";
+}
+
+function desafficher(){
+   document.getElementById("recherche").className = "";
+   document.getElementById("croix").className = "";
+   document.getElementById("loupe").className = "";
+}
